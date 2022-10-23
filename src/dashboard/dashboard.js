fb619cb875a1def2a249f970f056668d2acd4901
jeraruk <107743662+jeraruk@users.noreply.github.com>
2022-10-23 21:19:13 +1100

    integrate signup front/backend fix login navigate to homepage

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -6,26 +6,32 @@ import { Route, Routes } from "react-router-dom";
 import { logoutBrowser } from "../API/user-auth";
 const Dashboard = () => {
   const [user, setUser] = useState(null);
-  
+
   const logout = () => {
     setUser(null);
-    logoutBrowser()
+    logoutBrowser();
   };
 
   useEffect(() => {
-    if (localStorage.getItem('jwt')) {
-        console.log("found this json object", JSON.parse(localStorage.getItem('jwt')))
-        setUser(JSON.parse(localStorage.getItem('jwt')).user)
-      }
-  }, [])
+    if (localStorage.getItem("jwt")) {
+      console.log(
+        "found this json object",
+        JSON.parse(localStorage.getItem("jwt"))
+      );
+      setUser(JSON.parse(localStorage.getItem("jwt")).user);
+    }
+  }, []);
   return (
     <div>
       <Navigation user={user} logout={logout} />
       Dashboard
       <Routes>
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup user={user} />} />
 
-        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
+        <Route
+          path="/login"
+          element={<Login user={user} setUser={setUser} />}
+        />
       </Routes>
     </div>
   );
--- src/user/signup.js
+++ src/user/signup.js
@@ -1,8 +1,10 @@
 import React, { useRef, useState } from "react";
-import axios from "axios";
+import { register } from "../API/user-auth";
+import { useNavigate } from "react-router-dom";
 
-const Signup = () => {
+const Signup = ({ user }) => {
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   let registerData = {
     first_name: "",
@@ -17,19 +19,13 @@ const Signup = () => {
 
   const submitSignup = async (e) => {
     e.preventDefault();
-
-    await axios
-      .post(`http://localhost:3001/api/register`, registerData)
-      .then((response) => {
-        console.log(response.data);
-        if (response.error) {
-          console.log("error");
-          setError(response.error.message);
-          return;
-        } else {
-          setError(null);
-        }
-      });
+    try {
+      await register(registerData);
+      setError(null);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data.error);
+    }
 
     registerData = {
       first_name: "",
@@ -39,73 +35,87 @@ const Signup = () => {
     };
   };
 
-  return (
-    // create a login page and form, when user submits the form it sends a username, password, first_name, last_name
-    // copy this from the navigation page "submitLogin"
-    <div>
-      <form
-        onSubmit={(e) => submitSignup(e)}
-        className="flex flex-col items-center border max-w-[1440px] mx-auto px-6 py-12 space-y-4"
-      >
-        <div className="flex justify-center items-center space-x-3">
-          <label className="w-[80px]" for="">
-            First Name:
-          </label>
-          <input
-          required
-            className="border"
-            autoFocus
-            type="text"
-            onChange={(e) => updateFields("first_name", e.target.value)}
-          />
-        </div>
-        <div className="flex justify-center items-center space-x-3">
-          <label className="w-[80px]" for="">
-            Last Name:
-          </label>
-          <input
-          required
-            className="border"
-            type="text"
-            onChange={(e) => updateFields("last_name", e.target.value)}
-          />
-        </div>
+  if (user) {
+    return (
+      <div className="text-center py-16 text-2xl   ">
+        <p className="mb-16">You are already logged in.</p>
+        <a
+          href="/"
+          className="border px-10 py-10 rounded-full text-white bg-black"
+        >
+          Back to home page
+        </a>
+      </div>
+    );
+  } else {
+    return (
+      // create a login page and form, when user submits the form it sends a username, password, first_name, last_name
+      // copy this from the navigation page "submitLogin"
+      <div>
+        <form
+          onSubmit={(e) => submitSignup(e)}
+          className="flex flex-col items-center border max-w-[1440px] mx-auto px-6 py-12 space-y-4"
+        >
+          <div className="flex justify-center items-center space-x-3">
+            <label className="w-[80px]" for="">
+              First Name:
+            </label>
+            <input
+              required
+              className="border"
+              autoFocus
+              type="text"
+              onChange={(e) => updateFields("first_name", e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center items-center space-x-3">
+            <label className="w-[80px]" for="">
+              Last Name:
+            </label>
+            <input
+              required
+              className="border"
+              type="text"
+              onChange={(e) => updateFields("last_name", e.target.value)}
+            />
+          </div>
 
-        <div className="flex justify-center items-center space-x-3">
-          <label className="w-[80px]" for="">
-            Email:
-          </label>
-          <input
-          required
-            className="border"
-            type="email"
-            onChange={(e) => updateFields("email", e.target.value)}
-          />
-        </div>
-        <div className="flex justify-center items-center space-x-3">
-          <label className="w-[80px]" for="">
-            Password:
-          </label>
-          <input
-          required
-            className="border"
-            type="password"
-            onChange={(e) => updateFields("password", e.target.value)}
-          />
-        </div>
-        <div className={`${error ? "visible" : "invisible"} text-center m-0`}>
-          <span>Error: {error}</span>
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="border bg-green-400 hover:bg-green-300 px-7 py-2 rounded"
-          >
-            Sign Up
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+          <div className="flex justify-center items-center space-x-3">
+            <label className="w-[80px]" for="">
+              Email:
+            </label>
+            <input
+              required
+              className="border"
+              type="email"
+              onChange={(e) => updateFields("email", e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center items-center space-x-3">
+            <label className="w-[80px]" for="">
+              Password:
+            </label>
+            <input
+              required
+              className="border"
+              type="password"
+              onChange={(e) => updateFields("password", e.target.value)}
+            />
+          </div>
+          <div className={`${error ? "visible" : "invisible"} text-center m-0`}>
+            <span>Error: {error}</span>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="border bg-green-400 hover:bg-green-300 px-7 py-2 rounded"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 };
 export default Signup;
