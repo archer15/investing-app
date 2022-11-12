750034eb7f9a279e1079596c3e14ca7943bc323d
jeraruk <107743662+jeraruk@users.noreply.github.com>
2022-11-12 18:28:19 +1100

    set up investment popup

--- src/API/posting.js
+++ src/API/posting.js
@@ -1,39 +1,52 @@
 import axios from "axios";
-const URL = 'http://localhost:3001/'
+const URL = "http://localhost:3001/";
 
 export const fetch_posts = () => {
-    var posts
-    try {
-        posts = axios.get("http://localhost:3001/api/posts")
-        .then(response => {
-            //console.log(response.data)
-            if(response.error) {
-                console.log("error")
-                return
-            } else {
-                const validJobs = response.data
-                return validJobs
-            }
-        })
-    }
-    catch(error) {
-
-    }
-    return posts
-}
+  var posts;
+  try {
+    posts = axios.get("http://localhost:3001/api/posts").then((response) => {
+      //console.log(response.data)
+      if (response.error) {
+        console.log("error");
+        return;
+      } else {
+        const validJobs = response.data;
+        return validJobs;
+      }
+    });
+  } catch (error) {}
+  return posts;
+};
 export const list_post = (post) => {
-    
-    const log = axios.post(`http://localhost:3001/api/post`, post)
-    .then(response => {
-        //console.log(response.data)
-        if(response.error) {
-            console.log("error")
-            return
-        } else {
-            const posts = response.data
-            console.log(response.data)
-            return posts
-        }
-    })
-    return log
-}
+  const log = axios
+    .post(`http://localhost:3001/api/post`, post)
+    .then((response) => {
+      //console.log(response.data)
+      if (response.error) {
+        console.log("error");
+        return;
+      } else {
+        const posts = response.data;
+        console.log(response.data);
+        return posts;
+      }
+    });
+  return log;
+};
+
+export const invest_in_stock_post = (investment) => {
+  const log = axios
+    .put(`http://localhost:3001/api/invest`, investment)
+    .then((response) => {
+      //console.log(response.data)
+      if (response.error) {
+        console.log("error");
+        return;
+      } else {
+        const investment = response.data;
+        console.log(response.data);
+        return investment;
+      }
+    });
+  return log;
+};
